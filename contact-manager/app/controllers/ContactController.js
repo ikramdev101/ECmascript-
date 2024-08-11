aa46a05de4f3fb46f84b0f02454bf9d8defade47
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2024-08-11 18:33:02 +0200

    Create ContactController.js

---
+++ contact-manager/app/controllers/ContactController.js
@@ -0,0 +1,26 @@
+import Contact from '../models/Contact.js';
+import ContactView from '../views/ContactView.js';
+
+export default class ContactController {
+    constructor() {
+        this.contacts = [];
+        this.view = new ContactView();
+    }
+
+    init() {
+        this.view.bindAddContact(this.handleAddContact.bind(this));
+        this.view.bindDeleteContact(this.handleDeleteContact.bind(this));
+        this.view.renderContacts(this.contacts);
+    }
+
+    handleAddContact(name, phone) {
+        const newContact = new Contact(name, phone);
+        this.contacts.push(newContact);
+        this.view.renderContacts(this.contacts);
+    }
+
+    handleDeleteContact(name) {
+        this.contacts = this.contacts.filter(contact => contact.name !== name);
+        this.view.renderContacts(this.contacts);
+    }
+}
