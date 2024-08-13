9e7a8ae98987483b7c6ccd090c7b6b9b9efdee3c
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2024-08-13 17:20:51 +0200

    Create utils.js

---
+++ weather app/utils.js
@@ -0,0 +1,15 @@
+async function getCoordinates(city) {
+    const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(city)}&key=${apiKey}`);
+    if (!response.ok) {
+        throw new Error('Geocoding data could not be fetched');
+    }
+    const data = await response.json();
+    if (data.results.length > 0) {
+        return {
+            lat: data.results[0].geometry.lat,
+            lon: data.results[0].geometry.lng
+        };
+    } else {
+        throw new Error('City not found');
+    }
+}
