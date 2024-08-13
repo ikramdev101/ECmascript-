65c479e23de337d090a329748aadcf3fff4ce255
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2024-08-13 17:12:26 +0200

    Create controller.js

---
+++ weather app/controller.js
@@ -0,0 +1,34 @@
+import { getCoordinates } from 'utils.js'; 
+
+class WeatherController {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+
+        document.getElementById("cityInput").addEventListener("keyup", (event) => {
+            if (event.key === "Enter") {
+                this.getWeather();
+            }
+        });
+    }
+
+    async getWeather() {
+        const city = document.getElementById("cityInput").value;
+        if (city) {
+            try {
+                console.log('Fetching coordinates for:', city);
+                const { lat, lon } = await getCoordinates(city);
+                console.log('Coordinates:', lat, lon);
+                console.log('Fetching weather data for coordinates:', lat, lon);
+                const weatherData = await this.model.fetchWeather(lat, lon);
+                console.log('Weather data:', weatherData);
+                this.view.displayWeather(weatherData);
+            } catch (error) {
+                console.error('Error:', error.message);
+                this.view.displayError(error.message);
+            }
+        } else {
+            this.view.displayError("Please enter a city name.");
+        }
+    }
+}
