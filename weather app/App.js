4389a3f39cf125a8c161dfc652e3d8b3e2378631
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2024-08-13 17:10:32 +0200

    Create App.js

---
+++ weather app/App.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const model = new WeatherModel();
+    const view = new WeatherView();
+    const controller = new WeatherController(model, view);
+});
